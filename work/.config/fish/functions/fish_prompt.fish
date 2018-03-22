1a8d701838e04d3cabd3fe8df34b7988dd7f0ca1
sisto <sisto@sd.dk>
2018-03-22 13:56:07 +0100

    Add backup-work.fish and automated process

---
+++ work/.config/fish/functions/fish_prompt.fish
@@ -0,0 +1,11 @@
+function fish_prompt --description 'Write out the prompt'
+	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g') 
+	set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g') 
+	set -l prompt_symbol '>'
+        
+	if test (math (string length $pwd+$prompt_symbol+(date +%R)+$USER) + 15) -gt $COLUMNS
+	    set pwd (prompt_pwd)
+	end
+
+	printf "%s%s %s%s%s %s%s%s%s%s " (set_color $fish_color_normal) (date +%R) (set_color -u $fish_color_user) $USER (set_color normal) (set_color $fish_color_cwd) $pwd (set_color normal) (__fish_git_prompt) $prompt_symbol
+end
