baac3479fb18cff3ab4f97e7736ed5a4e7b85b1f
sisto <sisto@sd.dk>
2018-03-22 15:32:22 +0100

    2018-03-22 15:32

--- work/backup-work.fish
+++ work/backup-work.fish
@@ -3,7 +3,10 @@
 cp -rH -t ~/git/emacs-config/work ~/.config ~/backup-work.fish ~/move-to-zip.fish ~/check-status.fish ~/update.fish ~/.gitconfig ~/.gitignore ~/.emacs
 rm -r ~/git/emacs-config/work/.config/fish/generated_completions
 cd ~/git/emacs-config
-git add --all
-git commit -m (date +"%F %H:%M")
-git push
+if test -n (git status -s)
+    echo "Pushing to git"
+    git add --all
+    git commit -m (date +"%F %H:%M")
+    git push
+end
 
