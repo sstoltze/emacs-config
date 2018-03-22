bc92af7c1734549b7d146bbaf2e9a8073e2b6192
sisto <sisto@sd.dk>
2018-03-22 14:14:42 +0100

    Add automatic git commit to backup

--- work/backup-work.fish
+++ work/backup-work.fish
@@ -2,5 +2,8 @@
 
 cp -rH -t ~/git/emacs-config/work ~/.config ~/backup-work.fish ~/move-to-zip.fish ~/check-status.fish ~/update.fish ~/.gitconfig ~/.gitignore ~/.emacs
 rm -r ~/git/emacs-config/work/.config/fish/generated_completions
-
+cd ~/git/emacs-config
+git add --all
+git commit
+git push
 
