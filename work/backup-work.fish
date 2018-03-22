e5579d05092e2d6ca1b027d6ee58d049162d77b5
sisto <sisto@sd.dk>
2018-03-22 14:01:11 +0100

    Add update.fish to backup process

--- work/backup-work.fish
+++ work/backup-work.fish
@@ -1,6 +1,6 @@
 #!/usr/bin/fish
 
-cp -rH -t ~/git/emacs-config/work ~/git/git-standup ~/.config ~/backup-work.fish ~/move-to-zip.fish ~/check-status.fish ~/.gitconfig ~/.gitignore ~/.emacs
+cp -rH -t ~/git/emacs-config/work ~/.config ~/backup-work.fish ~/move-to-zip.fish ~/check-status.fish ~/update.fish ~/.gitconfig ~/.gitignore ~/.emacs
 rm -r ~/git/emacs-config/work/.config/fish/generated_completions
 
 
---
+++ work/update.fish
@@ -0,0 +1,2 @@
+#!/bin/fish
+/cygdrive/c/users/sisto/downloads/setup-x86_64.exe --no-desktop --no-shortcuts --no-startmenu --quiet-mode --upgrade-also
