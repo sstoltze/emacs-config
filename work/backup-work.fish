90dc98b80efdd1bb7f572f559c4096fa5ae7c1f2
sisto <sisto@sd.dk>
2018-04-17 09:35:58 +0200

    2018-04-17 09:35

--- work/backup-work.fish
+++ work/backup-work.fish
@@ -1,5 +1,25 @@
 #!/usr/bin/fish
 
+function handle_git
+    set -l gitstatus (git status -s)
+    if test -n "$gitstatus"
+        echo $gitstatus
+        echo "Adding and pushing to git."
+        git add --all
+        git commit -m (date +"%F %H:%M")
+        git push
+    else
+        echo "No changes, nothing to commit."
+    end
+end
+
+
+cd ~/noter
+echo "Noter:"
+handle_git
+cd ~
+
+echo "emacs-config:"
 cp -rH -t ~/git/emacs-config/work ~/.config ~/backup-work.fish ~/move-to-zip.fish ~/check-status.fish ~/update.fish ~/.gitconfig ~/.gitignore ~/.emacs ~/color-test.fish /cygdrive/c/Users/sisto/Pictures/background ~/prog
 rm -r ~/git/emacs-config/work/.config/fish/generated_completions
 cd ~/git/emacs-config
