943775b77440fa3da89a6579cee705688bf18658
sstoltze <sstoltze@gmail.com>
2018-09-21 13:47:03 +0200

    Add -nw to magit command

--- fish/.config/fish/functions/magit.fish
+++ fish/.config/fish/functions/magit.fish
@@ -1,3 +1,3 @@
 function magit
-    emacs --eval "(let ((pop-up-windows nil)) (call-interactively #'magit-status))"
+    emacs -nw --eval "(let ((pop-up-windows nil)) (call-interactively #'magit-status))"
 end
