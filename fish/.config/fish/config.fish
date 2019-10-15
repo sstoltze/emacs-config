a1e98e1d5177ab19fcad30cc3bb6be8ac6436f75
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2019-10-15 16:04:53 +0200

    Update theming and fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -22,7 +22,7 @@ set __fish_git_prompt_showdirtystate          'yes'
 set __fish_git_prompt_showstashstate          'yes'
 set __fish_git_prompt_showupstream            'yes'
 set __fish_git_prompt_show_informative_status 'yes'
-#set __fish_git_prompt_showuntrackedfiles     'yes'
+set __fish_git_prompt_showuntrackedfiles     'yes'
 #set __fish_git_prompt_color_upstream_ahead   green
 #set __fish_git_prompt_color_upstream_behind  red
 #set __fish_git_prompt_color_branch           $dark_pink
