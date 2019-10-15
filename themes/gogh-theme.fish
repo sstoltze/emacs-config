025c71895a244327f9d6e4817be2a02bb1261023
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2019-10-15 14:44:42 +0200

    Update gogh-theme to work correctly

--- themes/gogh-theme.fish
+++ themes/gogh-theme.fish
@@ -11,21 +11,33 @@ else
 end
 
 # Xresources
-awk 'BEGIN {OFS=""}
+awk -v xtheme="$theme.Xresources" -v kittyconf="$theme.conf" 'BEGIN {OFS=""}
 /COLOR/ {
 gsub(/"/,"",$2);
-gsub(/=/,": ", $2);
 gsub(/COLOR_0*/,"color",$2);
 gsub(/_COLOR/,"",$2);
-print "*.", tolower($2)
-}' "$theme.sh" > "$theme.Xresources"
+split($2,a,"=");
+v=tolower(a[1])
 
-# Kitty
-awk 'BEGIN {OFS=""}
-/COLOR/ {
-gsub(/"/,"",$2);
-gsub(/=/," ", $2);
-gsub(/COLOR_0*/,"color",$2);
-gsub(/_COLOR/,"",$2);
-print tolower($2)
-}' "$theme.sh" > "$theme.conf"
+if ( substr(a[2],1,1) == "$" ) {
+val=var[tolower(substr(a[2],2))];
+}
+else {
+    val=tolower(a[2]);
+}
+var[v]=val;
+
+if ( v ~ /color[0-9]+/ ) {
+number=substr(v,match(v,/[0-9]+/))-1
+v="color" number
+}
+
+xv=v;
+xval=val;
+gsub(/cursor/,"cursorColor",xv);
+print "*.", xv, ": ", xval > xtheme;
+
+kittyv=v;
+kittyval=val;
+print kittyv, " ", kittyval > kittyconf;
+}' "$theme.sh"
