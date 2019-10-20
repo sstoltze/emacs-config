90330bccda729590247ae58a87bf45f40b2f315c
Simon Stoltze <Simon.Stoltze@ufst.dk>
2019-10-21 00:40:41 +0200

    Fix whitespace in config.fish

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,8 +1,8 @@
 function fish_prompt --description 'Write out the prompt'
-	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g') 
-	set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g') 
+	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
+	set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
 	set -l prompt_symbol '>'
-        
+
 	if test (math (string length $pwd+$prompt_symbol+(date +%R)+$USER) + 15) -gt $COLUMNS
 	    set pwd (prompt_pwd)
 	end
