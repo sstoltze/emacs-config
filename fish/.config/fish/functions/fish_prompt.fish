4e6835280b130567a5ba43951c9d726d0e199391
Simon Stoltze <Simon.Stoltze@ufst.dk>
2019-10-21 15:49:39 +0200

    Small change to fix ligature-errors with ~ in fira code

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,7 +1,7 @@
 function fish_prompt --description 'Write out the prompt'
 	set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
 	set -l pwd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
-	set -l prompt_symbol '>'
+	set -l prompt_symbol ' >'
 
 	if test (math (string length $pwd+$prompt_symbol+(date +%R)+$USER) + 15) -gt $COLUMNS
 	    set pwd (prompt_pwd)
