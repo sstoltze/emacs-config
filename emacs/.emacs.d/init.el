466e915eb5d14d5c1772057b8ab5b57d74f7a73d
sstoltze <sstoltze@gmail.com>
2019-10-29 00:23:11 +0100

    Add dired-sidebar to config

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -636,6 +636,10 @@ If ARG is provided, move directly to option ARG."
 (use-package dired-async
   :after dired)
 
+(use-package dired-sidebar
+  :ensure t
+  :bind (("C-c j" . dired-sidebar-toggle-sidebar)))
+
 ;;;; --- Proced ---
 ;; To highlight processes use highlight-lines-matching-regexp, M-s h l
 ;; Unhighlight by unhighlight-regexp, M-s h u
