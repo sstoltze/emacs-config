0a5fe5d1576e9cda53cb8acdc9982f25efeb2f13
Simon Stoltze <Simon.Stoltze@ufst.dk>
2019-12-04 16:04:58 +0100

    Remove leiningen user plugin

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -2,4 +2,5 @@
                   [refactor-nrepl "2.5.0-SNAPSHOT"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
  ;; Install graphviz and run 'lein ns-dep-graph' in a project
- :user {:plugins [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]]}}
+ ;; :user {:plugins [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]]}
+ }
