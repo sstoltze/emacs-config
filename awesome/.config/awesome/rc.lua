ca3f72ee700d7c25b6f6d38a950f4905acb580c1
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2020-01-23 13:41:04 +0100

    Add /etc/default/keyboard, move ergodox setup to a separate file

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -489,7 +489,7 @@ local globalkeys = awful.util.table.join(
       {description = "increase the number of columns", group = "layout"}),
    awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1, nil, true)    end,
       {description = "decrease the number of columns", group = "layout"}),
-   awful.key({ modkey,           }, "space", function () awful.layout.inc( 1)                end,
+   awful.key({ modkey, "Control" }, "space", function () awful.layout.inc( 1)                end,
       {description = "select next", group = "layout"}),
    awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(-1)                end,
       {description = "select previous", group = "layout"}),
