885ed91b130fc2d72f051c0549dfe4eb96ec8a2a
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2020-01-27 12:01:42 +0100

    Update to fish 3

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -7,5 +7,5 @@ function fish_prompt --description 'Write out the prompt'
 	    set pwd (prompt_pwd)
 	end
 
-	printf "%s%s %s%s%s %s%s%s%s%s " (set_color $fish_color_normal) (date +%R) (set_color -u $fish_color_user) $USER (set_color normal) (set_color $fish_color_cwd) $pwd (set_color normal) (__fish_git_prompt) $prompt_symbol
+	printf "%s%s %s%s%s %s%s%s%s%s " (set_color $fish_color_normal) (date +%R) (set_color -u $fish_color_user) $USER (set_color normal) (set_color $fish_color_cwd) $pwd (set_color normal) (__fish_vcs_prompt) $prompt_symbol
 end
