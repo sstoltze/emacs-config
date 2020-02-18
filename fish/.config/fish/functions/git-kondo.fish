39c511fbea1be9f29f9cab795109bc61fb6bf597
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2020-02-18 14:42:57 +0100

    Add description to git-kondo

--- fish/.config/fish/functions/git-kondo.fish
+++ fish/.config/fish/functions/git-kondo.fish
@@ -1,4 +1,4 @@
-function git-kondo
+function git-kondo --description "Runs clj-kondo in the git project root"
     set -l start_dir (pwd)
     cd (git rev-parse --show-toplevel)
     set -l files (git diff (git merge-base origin/master HEAD) --name-only | grep -e '.clj[sc]*$')
