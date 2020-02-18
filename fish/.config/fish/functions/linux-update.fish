f6aae6e77a25987cd51e1346f4cb1df0b2396159
Simon Stoltze <Simon.Stoltze@ufst.dk>
2020-02-18 14:41:17 +0100

    Cleanup fish functions

--- fish/.config/fish/functions/linux-update.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 0
-function linux-update --description 'alias update sudo apt-get update; sudo apt-get -y dist-upgrade; sudo apt-get -y autoremove; sudo apt-get -y autoclean'
-	sudo apt-get update; sudo apt-get -y dist-upgrade; sudo apt-get -y autoremove; sudo apt-get -y autoclean $argv;
-end
--- fish/.config/fish/functions/start-bluetooth.fish
+++
@@ -1,5 +0,0 @@
-#/usr/fish
-function start-bluetooth
-    sudo modprobe btusb
-    sudo service bluetooth restart
-end
