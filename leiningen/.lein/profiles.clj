f687e340c9e7a5aa6c452b5532459ca6cb06bb79
Simon Stoltze <Simon.Stoltze@ufst.dk>
2020-03-04 09:43:14 +0100

    Update refactor-nrepl version

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,5 +1,5 @@
 {:repl {:plugins [[cider/cider-nrepl "0.24.0-SNAPSHOT"]
-                  [refactor-nrepl "2.5.0-SNAPSHOT"]]
+                  [refactor-nrepl "2.5.0"]]
         :dependencies [[org.clojure/tools.nrepl "0.2.13"]]}
  ;; Install graphviz and run 'lein ns-dep-graph' in a project
  ;; :user {:plugins [[lein-ns-dep-graph "0.2.0-SNAPSHOT"]]}
