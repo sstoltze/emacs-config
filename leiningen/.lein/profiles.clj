917b233a58a3ba766970d9407ff5ff88aa0a88a9
Simon Stoltze <Simon.Stoltze@ufst.dk>
2020-07-15 14:04:03 +0200

    Add nrepl dependency to allow connecting to external 'lein repl'

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,2 +1,3 @@
-{:user {:dependencies [[vvvvalvalval/scope-capture "0.3.2"] [hashp "0.2.0"]]
+{:repl {:plugins [[cider/cider-nrepl "0.25.2"]]}
+ :user {:dependencies [[vvvvalvalval/scope-capture "0.3.2"] [hashp "0.2.0"]]
         :injections [(require 'sc.api) (require 'hashp.core)]}}
