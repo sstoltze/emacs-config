a18ef42cb98cc742164e304ba2a3df5c6ca5c859
Sarah Stoltze <Simon.Stoltze@ufst.dk>
2020-07-30 11:03:09 +0200

    Update cider nrepl version

--- leiningen/.lein/profiles.clj
+++ leiningen/.lein/profiles.clj
@@ -1,3 +1,3 @@
-{:repl {:plugins [[cider/cider-nrepl "0.25.2"]]}
+{:repl {:plugins [[cider/cider-nrepl "0.25.3-SNAPSHOT"]]}
  :user {:dependencies [[vvvvalvalval/scope-capture "0.3.2"] #_[hashp "0.2.0"]]
         :injections [(require 'sc.api) #_(require 'hashp.core)]}}
