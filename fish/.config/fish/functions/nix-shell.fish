feb258edf6c25b2c16e170f246f3aa289754056e
sstoltze <sstoltze@gmail.com>
2021-01-16 19:06:59 +0100

    Rename nix-shell alias to nix-fish to fix issues

--- fish/.config/fish/functions/nix-shell.fish
+++
@@ -1,4 +0,0 @@
-# Defined in - @ line 1
-function nix-shell --wraps='nix-shell -p fish --run fish' --description 'alias nix-shell nix-shell -p fish --run fish'
- command nix-shell -p fish --run fish $argv;
-end
