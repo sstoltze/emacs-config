847296e8d74b5e0e4837f2bc114afda30a490ffb
sstoltze <sstoltze@gmail.com>
2021-02-08 15:46:15 +0100

    Add kubeon/kubeoff commands to enable/disable k8s status in prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -4,17 +4,33 @@ function fish_prompt --description 'Write out the prompt'
         set prompt_color "red"
     end
 
+    set -l time (printf "%s%s%s" (set_color $prompt_color) (date +%R) (set_color normal))
+
+    set -l user (printf "%s$USER%s" (set_color -u $fish_color_user) (set_color normal))
+
+    # Working directory
     set -l home_escaped (echo -n $HOME | sed 's/\//\\\\\//g')
     set -l wd (echo -n $PWD | sed "s/^$home_escaped/~/" | sed 's/ /%20/g')
-    set -l prompt_symbol ' >'
 
+    # Shorten when too long
     if test (math (string length $pwd+$prompt_symbol+(date +%R)+$USER) + 15) -gt $COLUMNS
         set wd (prompt_pwd)
     end
 
+    # Include nix shell when appropriate
     if test -n "$IN_NIX_SHELL"
         set wd (printf "%s[nix-shell:$wd]%s" (set_color $fish_color_nix) (set_color normal))
     end
 
-    printf "%s%s %s%s%s %s%s%s%s%s%s%s " (set_color $prompt_color) (date +%R) (set_color -u $fish_color_user) $USER (set_color normal) (set_color $fish_color_cwd) $wd (set_color normal) (__fish_vcs_prompt) (set_color $prompt_color) $prompt_symbol (set_color normal)
+    set wd (printf "%s$wd%s%s" (set_color $fish_color_cwd) (set_color normal) (__fish_vcs_prompt))
+
+    # Include k8s context when enabled by 'kubeon'
+    if test -n "$K8S_FISH_PROMPT"
+        set wd (printf "%s[k8s:%s]%s $wd" (set_color $fish_color_kubernetes) (kubectl config current-context) (set_color normal))
+    end
+
+    # The space before the '>' is needed due to aggresive ligatures
+    set -l prompt_symbol (printf "%s>%s " (set_color $prompt_color) (set_color normal))
+
+    printf "%s %s %s %s" $time $user $wd $prompt_symbol
 end
---
+++ fish/.config/fish/functions/kubeoff.fish
@@ -0,0 +1,3 @@
+function kubeoff --description 'Turns off kubernetes in prompt'
+    set -Ue K8S_FISH_PROMPT
+end
