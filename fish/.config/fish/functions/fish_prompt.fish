5392d55e7511c9e431e7eb6e2d089778d3985b95
Sarah Stoltze <sstoltze@gmail.com>
2022-01-03 13:17:07 +0100

    Update screen prompt

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -17,6 +17,11 @@ function fish_prompt --description 'Write out the prompt'
         set wd (prompt_pwd)
     end
 
+    # Include screen info when appropriate
+    if test (string sub -l 6 "$TERM") = 'screen'
+        set wd (printf "%s[screen:$wd]%s" (set_color $fish_color_nix) (set_color normal))
+    end
+
     # Include nix shell when appropriate
     if test -n "$IN_NIX_SHELL"
         set wd (printf "%s[nix-shell:$wd]%s" (set_color $fish_color_nix) (set_color normal))
