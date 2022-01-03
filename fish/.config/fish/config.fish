4144884592abe3bf4eeb1c94a3f274cf995809a2
Sarah Stoltze <sstoltze@gmail.com>
2022-01-03 11:40:47 +0100

    Fix executable test

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -51,12 +51,12 @@ end
 set -x DOCKER_BUILDKIT 1
 
 # fnm - cargo install fnm
-if test -x (which fnm)
+if command -v fnm
     fnm env --shell=fish | source
 end
 
 # AWS
-if test -x (which aws_completer)
+if command -v aws_completer
     complete --command aws --no-files --arguments '(begin; set --local --export COMP_SHELL fish; set --local --export COMP_LINE (commandline); aws_completer | sed \'s/ $//\'; end)'
 end
 
@@ -91,7 +91,7 @@ if test "$USER" = "sst"
             return $ret
         end
     end
-    if test -x (which direnv)
+    if command -v direnv
         direnv hook fish | source
     end
 end
