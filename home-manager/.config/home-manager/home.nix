5ddcf1ad0bfa24e68b8fe4a41e76315de2d68b65
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-09-12 16:56:59 +0200

    Try adding config files to home-manager setup

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -27,8 +27,10 @@ in
     pkgs.fish
     pkgs.iosevka
     pkgs.iosevka-bin
+    pkgs.emacs29
     pkgs.jq
     pkgs.git
+    pkgs.direnv
 
     # Kubie
     pkgs.kubie
@@ -37,28 +39,15 @@ in
     # Elixir
     elixir
     (pkgs.elixir-ls.override { elixir = elixir; })
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
+    ".emacs.d/init.el".source = ../../../emacs/.emacs.d/init.el;
+    ".gitconfig".source = ../../../git/.gitconfig;
+    ".config/git".source = ../../../git/.config/git;
+    ".config/fish".source = ../../../fish/.config/fish;
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
     # # symlink to the Nix store copy.
