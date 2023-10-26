8c27ba29c86bdf6ea70aebf9afd629d5364980df
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-10-26 22:01:58 +0200

    Format and rework home.nix

--- home-manager/.config/home-manager/home.nix
+++ home-manager/.config/home-manager/home.nix
@@ -1,81 +1,81 @@
 { config, pkgs, ... }:
 
-let beamPackages =
-        with pkgs.beam_minimal;
-        packagesWith interpreters.erlangR26;
-    elixir_1_15 = beamPackages.elixir_1_15;
-in
+let
+  beamPackages = with pkgs.beam_minimal; packagesWith interpreters.erlangR26;
+  elixir_1_15 = beamPackages.elixir_1_15;
+in {
+  home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "sarah.stoltze";
+    homeDirectory = "/Users/sarah.stoltze";
 
-{
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "sarah.stoltze";
-  home.homeDirectory = "/Users/sarah.stoltze";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not change this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.05"; # Please read the comment before changing.
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.05"; # Please read the comment before changing.
+    # The home.packages option allows you to install Nix packages into your
+    # environment.
+    packages = with pkgs; [
+      stow
+      fish
+      iosevka
+      iosevka-bin
+      emacs29
+      jq
+      git
+      direnv
+      kitty
+      dbeaver
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    stow
-    fish
-    iosevka
-    iosevka-bin
-    emacs29
-    jq
-    git
-    direnv
-    kitty
-    dbeaver
+      # Kubie
+      kubie
+      kubelogin
 
-    # Kubie
-    kubie
-    kubelogin
+      # Elixir
+      elixir_1_15
+      (pkgs.elixir-ls.override { elixir = elixir_1_15; })
+    ];
 
-    # Elixir
-    elixir_1_15
-    (pkgs.elixir-ls.override { elixir = elixir_1_15; })
-  ];
+    # Home Manager is pretty good at managing dotfiles. The primary way to manage
+    # plain files is through 'home.file'.
+    # home.file = {
+    #   ".emacs.d/init.el".source = ../../../emacs/.emacs.d/init.el;
+    #   ".gitconfig".source = ../../../git/.gitconfig;
+    #   ".config/git".source = ../../../git/.config/git;
+    #   ".config/fish".source = ../../../fish/.config/fish;
+    #   ".config/kitty".source = ../../../kitty/.config/kitty;
+    #   # Building this configuration will create a copy of 'dotfiles/screenrc' in
+    #   # the Nix store. Activating the configuration will then make '~/.screenrc' a
+    #   # symlink to the Nix store copy.
+    #   ".screenrc".source = dotfiles/screenrc;
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
-  # home.file = {
-  #   ".emacs.d/init.el".source = ../../../emacs/.emacs.d/init.el;
-  #   ".gitconfig".source = ../../../git/.gitconfig;
-  #   ".config/git".source = ../../../git/.config/git;
-  #   ".config/fish".source = ../../../fish/.config/fish;
-  #   ".config/kitty".source = ../../../kitty/.config/kitty;
-  #   # Building this configuration will create a copy of 'dotfiles/screenrc' in
-  #   # the Nix store. Activating the configuration will then make '~/.screenrc' a
-  #   # symlink to the Nix store copy.
-  #   ".screenrc".source = dotfiles/screenrc;
+    #   # You can also set the file content immediately.
+    #   ".gradle/gradle.properties".text = ''
+    #     org.gradle.console=verbose
+    #     org.gradle.daemon.idletimeout=3600000
+    #   '';
+    # };
 
-  #   # You can also set the file content immediately.
-  #   ".gradle/gradle.properties".text = ''
-  #     org.gradle.console=verbose
-  #     org.gradle.daemon.idletimeout=3600000
-  #   '';
-  # };
+    # You can also manage environment variables but you will have to manually
+    # source
+    #
+    #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+    #
+    # or
+    #
+    #  /etc/profiles/per-user/sarah.stoltze/etc/profile.d/hm-session-vars.sh
+    #
+    # if you don't want to manage your shell through Home Manager.
+    sessionVariables = {
+      # EDITOR = "emacs";
+    };
 
-  # You can also manage environment variables but you will have to manually
-  # source
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/sarah.stoltze/etc/profile.d/hm-session-vars.sh
-  #
-  # if you don't want to manage your shell through Home Manager.
-  home.sessionVariables = {
-    # EDITOR = "emacs";
   };
 
   # Let Home Manager install and manage itself.
