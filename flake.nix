841b5092051c5f986207c727015bf914286adf61
Sarah Ella Stoltze <sstoltze@gmail.com>
2023-12-31 17:01:47 +0100

    Add credoLanguageServer to elixirSetup

--- flake.nix
+++ flake.nix
@@ -10,16 +10,32 @@
     elixirSetup = flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        systemPackages = if (builtins.any (s: s == system) [
-          "x86_64-darwin"
-          "aarch64-darwin"
-        ]) then
-          [ ]
-        else
-          [ pkgs.inotify-tools ];
+        credoLanguageServer = pkgs.stdenv.mkDerivation {
+          name = "credo-language-server";
+
+          src = pkgs.fetchFromGitHub {
+            owner = "elixir-tools";
+            repo = "credo-language-server";
+            # Release 0.3.0
+            rev = "106be2476073d5ec85ed1695dc2b11e94abf650a";
+            hash = "sha256-YrwLzQTwtMNiaIT0Ar59PwsT0q+lhAVNvBKAlewHZ2Y=";
+          };
+
+          installPhase = ''
+            mkdir -p "$out"/bin
+            cp -a "$src"/bin/credo-language-server "$out"/bin/credo-language-server
+            chmod +x "$out"/bin/credo-language-server
+          '';
+
+          dontFixup = true;
+        };
       in {
+        credoLanguageServer = credoLanguageServer;
         devShell = pkgs.mkShell {
-          packages = with pkgs; [ elixir elixir_ls sqlite ] ++ systemPackages;
+          packages = with pkgs;
+            [ elixir elixir_ls sqlite credoLanguageServer ]
+            ++ pkgs.lib.optional stdenv.isLinux inotify-tools
+            ++ pkgs.lib.optional stdenv.isDarwin terminal-notifier;
         };
       });
   };
