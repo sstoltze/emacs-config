7ee75d05b9b5c9c84843a0fe13fa51047a19c1d7
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-04-04 19:34:39 +0200

    Update nixpkgs url for nixos

--- nixos/etc/nixos/flake.nix
+++ nixos/etc/nixos/flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    # nixpkgs = { url = "github:NixOS/nixpkgs"; };
+    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
     emacsConfig = {
       url = "/home/sst/git/emacs-config";
       inputs.nixpkgs.follows = "nixpkgs";
