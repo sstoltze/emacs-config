5417e1719db5b2cb4bcee832964e714e0e7084db
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-06-10 10:31:56 +0200

    Use 2 spaces for tabs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -167,7 +167,7 @@
 ;; Do not use tabs
 (setq-default indent-tabs-mode              nil
               show-trailing-whitespace      t
-              tab-width                     4
+              tab-width                     2
               tab-always-indent             'complete)
 
 (set-face-background 'trailing-whitespace "IndianRed4")
