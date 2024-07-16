af7b4718fa0381368ef157e2e698bae2eabca315
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-07-16 09:20:08 +0200

    Update custom package setup

--- nix-files/emacs/custom-packages.nix
+++ nix-files/emacs/custom-packages.nix
@@ -1,13 +1,29 @@
 { pkgs, ... }:
+
 {
   programs.emacs = {
-    extraPackages = epkgs: [
-      (pkgs.callPackage
-        ./packages/related-files.nix
-        { inherit (pkgs) fetchFromGitHub; inherit (epkgs) trivialBuild; })
-      (pkgs.callPackage
-        ./packages/document-sections.nix
-        { inherit (pkgs) fetchFromGitHub; inherit (epkgs) trivialBuild; })
-    ];
+    extraConfig = ''
+      (use-package related-files
+        :bind (("C-c r" . related-files-find-related-file)))
+
+      (use-package document-sections
+        :bind (("C-c d" . document-sections-find-section)))
+    '';
+
+    extraPackages = epkgs:
+      let
+        relatedFiles = pkgs.callPackage ./packages/related-files.nix {
+          inherit (pkgs) fetchFromGitHub;
+          inherit (epkgs) trivialBuild;
+        };
+        documentSections = pkgs.callPackage ./packages/document-sections.nix {
+          inherit (pkgs) fetchFromGitHub;
+          inherit (epkgs) trivialBuild;
+        };
+      in
+      [
+        relatedFiles
+        documentSections
+      ];
   };
 }
