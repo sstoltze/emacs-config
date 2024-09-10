ee1da89ed6c86f4af46712159690c4c04812fe18
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-09-10 11:26:56 +0200

    add kitty config, like colours

--- nix-files/kitty.nix
+++ nix-files/kitty.nix
@@ -8,5 +8,34 @@
       name = pkgs.lib.mkForce "Iosevka Term";
       size = 14;
     };
+    keybindings = {
+      "ctrl+shift+o" = "layout_action rotate";
+      "ctrl+shift+up" = "move_window up";
+      "ctrl+shift+down" = "move_window down";
+      "ctrl+shift+left" = "move_window left";
+      "ctrl+shift+right" = "move_window right";
+    };
+    settings = {
+      # aci-modified.conf
+      color0 = "#363636";
+      color1 = "#cd5c5c";
+      color2 = "#138034";
+      color3 = "#eedd82";
+      color4 = "#0883ff";
+      color5 = "#cd00cd";
+      color6 = "#98f5ff";
+      color7 = "#cccccc";
+      color8 = "#424242";
+      color9 = "#ff0000";
+      color10 = "#9acd32";
+      color11 = "#ffed1e";
+      color12 = "#1e8eff";
+      color13 = "#8e1eff";
+      color14 = "#7ac5cd";
+      color15 = "#c2c2c2";
+      background = "#181a26";
+      foreground = "#cccccc";
+      cursor = "#eedd82";
+    };
   };
 }
