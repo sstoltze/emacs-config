3488b7e8c6a1c785f701488b5cf04bb3e5597ef7
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-09-13 12:01:53 +0200

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,10 +1,16 @@
 {
   description = "Gathering various flakes and nix/nixos configs";
   inputs = {
-    flake-utils = { url = "github:numtide/flake-utils"; };
     nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
   };
-  outputs = { flake-utils, nixpkgs, ... }:
+  outputs = { nixpkgs, ... }:
+    let
+      forEachSystem = (f: nixpkgs.lib.genAttrs [
+        "aarch64-darwin"
+        "x86_64-linux"
+      ]
+        (system: f system));
+    in
     {
       nixosModules = {
         default = import ./nix-files/nixos-configuration.nix;
@@ -12,23 +18,24 @@
       };
 
       # Basic setup for an elixir flake
-      elixirSetup = flake-utils.lib.eachDefaultSystem (system:
-        let
-          pkgs = import nixpkgs { inherit system; };
-          credoLanguageServer =
-            pkgs.callPackage ./nix-files/credo-language-server.nix { };
-        in
-        {
-          devShell = pkgs.mkShell {
-            buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
-            packages = with pkgs;
-              [ elixir elixir_ls sqlite credoLanguageServer ]
-              ++ lib.optional stdenv.isLinux inotify-tools
-              ++ lib.optional stdenv.isDarwin terminal-notifier
-              ++ lib.optional stdenv.isDarwin fswatch;
-          };
-        });
-      racketSetup = flake-utils.lib.eachDefaultSystem (system:
+      elixirSetup = forEachSystem
+        (system:
+          let
+            pkgs = import nixpkgs { inherit system; };
+            credoLanguageServer =
+              pkgs.callPackage ./nix-files/credo-language-server.nix { };
+          in
+          {
+            devShell = pkgs.mkShell {
+              buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
+              packages = with pkgs;
+                [ elixir elixir_ls sqlite credoLanguageServer ]
+                ++ lib.optional stdenv.isLinux inotify-tools
+                ++ lib.optional stdenv.isDarwin terminal-notifier
+                ++ lib.optional stdenv.isDarwin fswatch;
+            };
+          });
+      racketSetup = forEachSystem (system:
         let
           pkgs = import nixpkgs {
             inherit system;
@@ -41,7 +48,7 @@
             };
         }
       );
-      haskellSetup = flake-utils.lib.eachDefaultSystem (system:
+      haskellSetup = forEachSystem (system:
         let
           pkgs = import nixpkgs {
             inherit system;
