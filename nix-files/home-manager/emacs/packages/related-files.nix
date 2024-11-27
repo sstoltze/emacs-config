2a86955d0a5a67e87e69fbc185e92c2fa06090a8
Sarah Ella Stoltze <sstoltze@gmail.com>
2024-11-27 13:43:57 +0100

    Update related-files version
    Co-authored-by: Andrew Bruce <andrew.bruce@maersk.com>

--- nix-files/home-manager/emacs/packages/related-files.nix
+++ nix-files/home-manager/emacs/packages/related-files.nix
@@ -1,17 +1,18 @@
-{ trivialBuild
-, fetchFromGitHub
-, ...
+{
+  trivialBuild,
+  fetchFromGitHub,
+  ...
 }:
 
 trivialBuild {
   pname = "related-files";
-  version = "main-2024-07-15";
+  version = "main-2024-11-29";
 
   src = fetchFromGitHub {
     owner = "sstoltze";
     repo = "related-files";
-    rev = "ac53bb55d8f7d5e02e731febaf13911fb13ec6ad";
-    sha256 = "sha256-tw+t8EU95bzsTpS3Let6IUKoUK0xEGogVySacP08IuQ=";
+    rev = "2a904d6c8b3701e900c0a12f493ffd1a92906392";
+    sha256 = "sha256-xTkRXEz+AHJ4UWo6LrQv/eQFtcSfQFiLHe6DazVMYHU=";
   };
 
 }
