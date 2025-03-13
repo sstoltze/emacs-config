81bea3cfe438a7fc1aaa1e0c15cadbb5fc0028aa
Sarah Ella Stoltze <sstoltze@gmail.com>
2025-03-13 12:00:16 +0100

    Add lsp-tailwindcss

--- nix-files/home-manager/emacs/lsp.nix
+++ nix-files/home-manager/emacs/lsp.nix
@@ -1,12 +1,23 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   programs.emacs = {
-    extraConfig = builtins.readFile ./elisp/lsp.el;
-    extraPackages = epkgs: with epkgs; [
-      company
-      lsp-mode
-      lsp-ivy
-    ];
+    extraConfig = ''
+      ${builtins.readFile ./elisp/lsp.el}
+
+      (use-package lsp-tailwindcss
+        :after lsp-mode
+        :custom
+        (lsp-tailwindcss-server-path "${pkgs.lib.getExe pkgs.tailwindcss-language-server}")
+        :config
+        (add-to-list 'lsp-tailwindcss-major-modes 'elixir-ts-mode))
+    '';
+    extraPackages =
+      epkgs: with epkgs; [
+        company
+        lsp-mode
+        lsp-ivy
+        lsp-tailwindcss
+      ];
   };
 }
