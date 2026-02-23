b71ee83ff647c4cdf892d74f5eab7b70792d16ba
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-02-23 09:26:01 +0100

    Re-enable org-mode on macs

--- nix-files/home-manager/emacs/org.nix
+++ nix-files/home-manager/emacs/org.nix
@@ -1,15 +1,11 @@
 { pkgs, ... }:
 {
-  programs.emacs =
-    if pkgs.stdenv.isDarwin then
-      { }
-    else
-      {
-        extraConfig = builtins.readFile ./elisp/org.el;
-        extraPackages =
-          epkgs: with epkgs; [
-            org
-            org-tree-slide
-          ];
-      };
+  programs.emacs = {
+    extraConfig = builtins.readFile ./elisp/org.el;
+    extraPackages =
+      epkgs: with epkgs; [
+        org
+        org-tree-slide
+      ];
+  };
 }
