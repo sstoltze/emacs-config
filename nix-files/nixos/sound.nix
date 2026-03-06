cee2ef48fe3ca5ad68172eaf3e7192660d9420f2
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-03-06 18:27:54 +0100

    Revert "Try and fix HDMI sound"
    This reverts commit 78c803f4a6fd754d8eac57227685bd22fbeb08ed.
    Don't think this did anything

--- nix-files/nixos/sound.nix
+++ nix-files/nixos/sound.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 {
   # rtkit is optional but recommended
   security.rtkit.enable = true;
@@ -9,25 +9,5 @@
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
     jack.enable = true;
-    wireplumber.configPackages = [
-      (pkgs.writeTextDir "share/wireplumber/main.lua.d/99-fix-tv.lua" ''
-          alsa_monitor.rules = {
-          {
-            matches = {
-              {
-                -- Matches PHILIPS FTV.
-                { "alsa.name", "matches", "HDMI 0" },
-                { "node.name", "matches", "alsa_output.*" },
-              }
-            },
-            apply_properties = {
-            ["audio.format"]           = "S16LE",
-            ["audio.channels"]         = 2,
-            ["audio.position"]         = "FR,FL"
-            },
-          }
-        }
-      '')
-    ];
   };
 }
