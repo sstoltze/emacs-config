836a923c342bf2ded106e1f4da25fc1fba011676
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-04-01 09:33:57 +0200

    elixir-ls rename

--- flake.nix
+++ flake.nix
@@ -1,15 +1,28 @@
 {
   description = "Gathering various flakes and nix/nixos configs";
   inputs = {
-    nixpkgs = { url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
+    nixpkgs = {
+      url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    };
   };
-  outputs = { nixpkgs, ... }:
+  outputs =
+    { nixpkgs, ... }:
     let
-      forEachSystem = (f: nixpkgs.lib.genAttrs [
-        "aarch64-darwin"
-        "x86_64-linux"
-      ]
-        (system: f { inherit system; pkgs = import nixpkgs { inherit system; }; }));
+      forEachSystem = (
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-darwin"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            f {
+              inherit system;
+              pkgs = import nixpkgs { inherit system; };
+            }
+          )
+      );
     in
     {
       inherit nixpkgs;
@@ -21,49 +34,56 @@
       };
 
       # Basic setup for an elixir flake
-      elixirDevShells = forEachSystem
-        ({ pkgs, ... }:
-          let
-            credoLanguageServer =
-              pkgs.callPackage ./nix-files/credo-language-server.nix { };
-          in
-          {
-            default = pkgs.mkShell
-              {
-                buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
-                packages = with pkgs;
-                  [ elixir elixir_ls sqlite credoLanguageServer ]
-                  ++ lib.optional stdenv.isLinux inotify-tools
-                  ++ lib.optional stdenv.isDarwin terminal-notifier
-                  ++ lib.optional stdenv.isDarwin fswatch;
-              };
-          }
-        );
-
-      racketSetup = forEachSystem (system:
+      elixirDevShells = forEachSystem (
+        { pkgs, ... }:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-          };
+          credoLanguageServer = pkgs.callPackage ./nix-files/credo-language-server.nix { };
         in
         {
-          devShell = pkgs.mkShell
-            {
-              packages = [ pkgs.racket ];
-            };
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; lib.optional stdenv.isDarwin darwin.apple_sdk.frameworks.CoreServices;
+            packages =
+              with pkgs;
+              [
+                elixir
+                elixir-ls
+                sqlite
+                credoLanguageServer
+              ]
+              ++ lib.optional stdenv.isLinux inotify-tools
+              ++ lib.optional stdenv.isDarwin terminal-notifier
+              ++ lib.optional stdenv.isDarwin fswatch;
+          };
         }
       );
-      haskellSetup = forEachSystem (system:
+
+      racketSetup = forEachSystem (
+        system:
         let
           pkgs = import nixpkgs {
             inherit system;
           };
         in
         {
-          devShell = pkgs.mkShell
-            {
-              packages = [ pkgs.stack pkgs.ghc ];
-            };
+          devShell = pkgs.mkShell {
+            packages = [ pkgs.racket ];
+          };
+        }
+      );
+      haskellSetup = forEachSystem (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+        in
+        {
+          devShell = pkgs.mkShell {
+            packages = [
+              pkgs.stack
+              pkgs.ghc
+            ];
+          };
         }
       );
     };
