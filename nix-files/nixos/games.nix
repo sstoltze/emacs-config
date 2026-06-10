a757e60519e36493443f64a608abc575cd19d11f
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-06-10 22:03:09 +0200

    Add some stuff

--- nix-files/nixos/games.nix
+++ nix-files/nixos/games.nix
@@ -2,7 +2,138 @@
 {
   programs.nix-ld.enable = true;
   programs.nix-ld.libraries = with pkgs; [
-    # Add any missing dynamic libraries for unpackaged programs
-    # here, NOT in environment.systemPackages
+    zstd
+    stdenv.cc.cc
+    curl
+    openssl
+    attr
+    libssh
+    bzip2
+    libxml2
+    acl
+    libsodium
+    util-linux
+    xz
+    systemd
+
+    # My own additions
+    libxcomposite
+    libxtst
+    libxrandr
+    libxext
+    libx11
+    libxfixes
+    libGL
+    libva
+    pipewire
+    libxcb
+    libxdamage
+    libxshmfence
+    libxxf86vm
+    libelf
+
+    # Required
+    glib
+    gtk2
+
+    # Inspired by steam
+    # https://github.com/NixOS/nixpkgs/blob/master/pkgs/by-name/st/steam/package.nix#L36-L85
+    networkmanager
+    vulkan-loader
+    libgbm
+    libdrm
+    libxcrypt
+    coreutils
+    pciutils
+    zenity
+    # glibc_multi.bin # Seems to cause issue in ARM
+
+    # # Without these it silently fails
+    libxinerama
+    libxcursor
+    libxrender
+    libxscrnsaver
+    libxi
+    libSM
+    libICE
+    gnome2.GConf
+    nspr
+    nss
+    cups
+    libcap
+    SDL2
+    libusb1
+    dbus-glib
+    ffmpeg
+    # Only libraries are needed from those two
+    libudev0-shim
+
+    # needed to run unity
+    gtk3
+    icu
+    libnotify
+    gsettings-desktop-schemas
+    # https://github.com/NixOS/nixpkgs/issues/72282
+    # https://github.com/NixOS/nixpkgs/blob/2e87260fafdd3d18aa1719246fd704b35e55b0f2/pkgs/applications/misc/joplin-desktop/default.nix#L16
+    # log in /home/leo/.config/unity3d/Editor.log
+    # it will segfault when opening files if you don’t do:
+    # export XDG_DATA_DIRS=/nix/store/0nfsywbk0qml4faa7sk3sdfmbd85b7ra-gsettings-desktop-schemas-43.0/share/gsettings-schemas/gsettings-desktop-schemas-43.0:/nix/store/rkscn1raa3x850zq7jp9q3j5ghcf6zi2-gtk+3-3.24.35/share/gsettings-schemas/gtk+3-3.24.35/:$XDG_DATA_DIRS
+    # other issue: (Unity:377230): GLib-GIO-CRITICAL **: 21:09:04.706: g_dbus_proxy_call_sync_internal: assertion 'G_IS_DBUS_PROXY (proxy)' failed
+
+    # Verified games requirements
+    libxt
+    libxmu
+    libogg
+    libvorbis
+    SDL
+    SDL2_image
+    glew_1_10
+    libidn
+    tbb
+
+    # Other things from runtime
+    flac
+    freeglut
+    libjpeg
+    libpng
+    libpng12
+    libsamplerate
+    libmikmod
+    libtheora
+    libtiff
+    pixman
+    speex
+    SDL_image
+    SDL_ttf
+    SDL_mixer
+    SDL2_ttf
+    SDL2_mixer
+    libappindicator-gtk2
+    libdbusmenu-gtk2
+    libindicator-gtk2
+    libcaca
+    libcanberra
+    libgcrypt
+    libvpx
+    librsvg
+    libxft
+    libvdpau
+    # ...
+    # Some more libraries that I needed to run programs
+    pango
+    cairo
+    atk
+    gdk-pixbuf
+    fontconfig
+    freetype
+    dbus
+    alsa-lib
+    expat
+    # for blender
+    libxkbcommon
+
+    libxcrypt-legacy # For natron
+    libGLU # For natron
+
   ];
 }
