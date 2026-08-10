f9d1de6b0fc0b5f90c96c2692198661e107b4e6e
Sarah Ella Stoltze <sstoltze@gmail.com>
2026-08-10 14:09:07 +0200

    Fix nix formatting

--- nix-files/home-manager/emacs/nix.nix
+++ nix-files/home-manager/emacs/nix.nix
@@ -9,7 +9,7 @@
         :hook ((nix-mode . (lambda ()
                              (add-hook 'before-save-hook 'nix-format-before-save 0 t))))
         :custom
-        (nix-nixfmt-bin "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt"))
+        (nix-nixfmt-bin "${pkgs.nixpkgs-fmt}/bin/nixpkgs-fmt -"))
     '';
     extraPackages = epkgs: [ epkgs.nix-mode ];
   };
